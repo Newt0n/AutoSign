e9b61aa7c452ad58a86e827b1bbaf8380bd5b3e0
Newt0n <mciguu@gmail.com>
2013-04-29 13:19:36 +0800

    115 update fixed

--- services/oneonefive.php
+++ services/oneonefive.php
@@ -16,7 +16,7 @@ class OneOneFive extends Sign
 
 	//登录 URL
 	private $homeUrl = 'http://115.com/';
-	private $postUrl = 'http://passport.115.com/?ac=login&goto=http%3A%2F%2Fwww.115.com';
+	private $postUrl = 'http://passport.115.com/?ct=login&ac=ajax&is_ssl=1';
 
 	//签到 URL
 	private $signUrl = 'http://115.com/?ct=ajax_user&ac=pick_spaces&u=1&token=';
@@ -28,16 +28,23 @@ class OneOneFive extends Sign
 	{
 		if(!$this->isCookieExist)
 		{
+			$vcode = uniqid();
 			$data = array(
-				'login[account]'=>$this->username,
-				'login[passwd]'=>$this->password,
-				'login[time]'=>'on',
-				'back'=>'http://www.115.com'
+				'login[ssoent]'=>'A1',
+				'login[version]'=>'2.0',
+				'login[ssoext]'=>$vcode,
+				'login[ssoln]'=>$this->username,
+				'login[ssopw]'=>$this->sha1_pwd($this->password, $this->username, $vcode),
+				'login[ssovcode]'=>$vcode,
+				'login[time]'=>1,
+				'goto'=>''
 				);
-			$this->post($this->postUrl, $data);
-			
+			$httpheader = array(
+				'X-Requested-With:XMLHttpRequest'
+				);
+			$this->post($this->postUrl, $data, $httpheader);
 		}
-		
+
 		$homeResp = $this->get($this->homeUrl);
 		preg_match('/take_token:\s*\'([^\']*)/', $homeResp, $match);
 		if(empty($match[0]))
@@ -49,12 +56,20 @@ class OneOneFive extends Sign
 		}
 
 		$token = $match[1];
-		$signResp = $this->get($this->signUrl.$token);
+		$signResp = $this->get($this->signUrl.$token.'&_='.time());
 		$signResp = json_decode($signResp);
-		if(isset($signResp->state))
+		if(isset($signResp->picked))
 			$this->logLine .= self::SUCCESS.' 获得空间：'.$signResp->picked.' 总容量：'.$signResp->total_size;
 		else
+		{
+			@$this->logLine .= $signResp->msg;
 			$this->retry();
+		}
+	}
+
+	private function sha1_pwd($password, $username, $vcode)
+	{
+		return sha1(sha1(sha1($password).sha1($username)).strtoupper($vcode));
 	}
 }
 
