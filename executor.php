39f0b11ea861d94dd353d7c7a8592d7c63f5cca8
ZhangJin <jin.zhang@xiaomi.com>
2013-12-04 14:53:29 +0800

    tweak

--- executor.php
+++ executor.php
@@ -7,151 +7,144 @@ require 'sign.php';
  * @author Newton <mciguu@gmail.com>
  */
 class Executor
-{	
-	//错误次数
-	private $errCount = 0;
-	//日志文本
-	private $logText;
-	private $allSigned = true;
-	//Weibo 实例
-	private static $weibo = NULL;
-	private static $token = NULL;
+{   
+    //错误次数
+    private $errCount = 0;
+    //日志文本
+    private $logText;
+    private $allSigned = true;
+    //Weibo 实例
+    private static $weibo = NULL;
+    private static $token = NULL;
 
-	/**
-	 * 构造函数，记录时间戳并执行签到
-	 * @param array $accounts 需要签到的账户信息数组
-	 */
-	public function __construct($accounts)
-	{	
-		date_default_timezone_set('PRC');
-		$timestamp = date('Y-m-d H:i:s', time());
-		$this->logText = $timestamp.PHP_EOL;
-		echo $timestamp,'<br>';
+    /**
+     * 构造函数，记录时间戳并执行签到
+     * @param array $accounts 需要签到的账户信息数组
+     */
+    public function __construct($accounts)
+    {   
+        date_default_timezone_set('PRC');
+        $timestamp = date('Y-m-d H:i:s', time());
+        $this->logText = $timestamp.PHP_EOL;
+        echo $timestamp,'<br>';
 
-		set_time_limit(60);
-		$this->execute($accounts);
-	}
+        set_time_limit(60);
+        $this->execute($accounts);
+    }
 
 /**
  * 签到方法
  * @param array $accounts 需要签到的账户信息数组
  */
-	public function execute($accounts)
-	{
-		foreach ($accounts as $userInfo)
-		{
-			$svcName = $userInfo[0];
-			try
-			{
-				$this->fileLoader('services/'.strtolower($svcName).'.php'); //载入要签到服务的子类
-			}
-			catch (Exception $e)
-			{
-				die($e->getMessage());
-			}
+    public function execute($accounts)
+    {
+        foreach ($accounts as $userInfo)
+        {
+            $svcName = $userInfo[0];
+            try {
+                $this->fileLoader(DIR_ROOT.'/services/'.strtolower($svcName).'.php'); //载入要签到服务的子类
+            } catch (Exception $e) {
+                die($e->getMessage());
+            }
 
-			//获取实例并初始化
-			$instance = $svcName::getInstance();
-			$instance->init($userInfo[1], $userInfo[2]);
+            //获取实例并初始化
+            $instance = $svcName::getInstance();
+            $instance->init($userInfo[1], $userInfo[2]);
 
-			$errCount = $this->errCount;
-			while($errCount < RETRY_LIMIT)
-			{
-				try
-				{
-					$instance->sign();
-				}
-				catch (Exception $e)
-				{
-					$errCount++;
-					continue;
-				}
-				break;
-			}
+            $errCount = $this->errCount;
+            while($errCount < RETRY_LIMIT)
+            {
+                try {
+                    $instance->sign();
+                } catch (Exception $e) {
+                    $errCount++;
+                    continue;
+                }
 
-			if($errCount >= RETRY_LIMIT)
-			{
-				$this->allSigned = false;
-				if(NOTIFY_FAILED)
-					try
-					{
-						$status = $svcName.' 失败多次，请检查日志 #AutoSign#';
-						$this->weiboNotify($status);
-					}
-					catch (Exception $e)
-					{
-						$instance->appendLog($e->getMessage());	
-					}
-			}
+                break;
+            }
 
-			//记录日志
-			$this->logText .= $instance->getLog();
-		}
-		//输出日志
-		if(LOG)
-			$this->log();
+            if($errCount >= RETRY_LIMIT)
+            {
+                $this->allSigned = false;
+                if(NOTIFY_FAILED)
+                    try {
+                        $status = $svcName.' 失败多次，请检查日志 #AutoSign#';
+                        $this->weiboNotify($status);
+                    } catch (Exception $e) {
+                        $instance->appendLog($e->getMessage()); 
+                    }
+            }
 
-		if(NOTIFY_SUCCESS && $this->allSigned)
-			$this->weiboNotify('All signed', 2);
-	}
+            //记录日志
+            $this->logText .= $instance->getLog();
+        }
+        //输出日志
+        if(LOG)
+            $this->log();
 
-	/**
-	 * 发送微博通知
-	 * @param  string $status 要发布的微博内容
-	 */
-	private function weiboNotify($status, $visible = 0, $listId = NULL)
-	{
-		if(is_null(self::$weibo))
-		{
-			$this->fileLoader('weibo/config.php');
-			$this->fileLoader('weibo/saetv2.ex.class.php');
-			if(file_exists(OAUTH_FILE))
-			{
-				self::$token = file_get_contents(OAUTH_FILE);
-				self::$token = unserialize(self::$token);
-			}
-			else
-				throw new Exception("微博认证文件不存在");
+        if(NOTIFY_SUCCESS && $this->allSigned)
+            $this->weiboNotify('All signed', 2);
+    }
 
-			self::$weibo = new SaeTClientV2( WB_AKEY , WB_SKEY , self::$token['access_token']);
-		}
-		$weiboName = WEIBO_NAME;
-		if(empty($weiboName))
-			$weiboName = self::$token['name'];
+    /**
+     * 发送微博通知
+     * @param  string $status 要发布的微博内容
+     */
+    private function weiboNotify($status, $visible = 0, $listId = NULL)
+    {
+        if(is_null(self::$weibo))
+        {
+            $this->fileLoader('weibo/config.php');
+            $this->fileLoader('weibo/saetv2.ex.class.php');
+            if(file_exists(OAUTH_FILE))
+            {
+                self::$token = file_get_contents(OAUTH_FILE);
+                self::$token = unserialize(self::$token);
+            }
+            else
+                throw new Exception("微博认证文件不存在");
 
-		$status = '@'.$weiboName.' '.$status;
-		$resp = self::$weibo->update($status, $visible, $listId);
-		if(isset($resp->error_code))
-			throw new Exception($resp->error);
-	}
+            self::$weibo = new SaeTClientV2( WB_AKEY , WB_SKEY , self::$token['access_token']);
+        }
+        $weiboName = WEIBO_NAME;
+        if(empty($weiboName))
+            $weiboName = self::$token['name'];
 
-	/**
-	 * 输出日志到文件
-	 */
-	private function log()
-	{
-		file_put_contents('sign.log', $this->logText, FILE_APPEND);
-	}
+        $status = '@'.$weiboName.' '.$status;
+        $resp = self::$weibo->update($status, $visible, $listId);
+        if(isset($resp->error_code))
+            throw new Exception($resp->error);
+    }
 
-	/**
-	 * 返回本次签到状态
-	 * @return boolean 全部成功: True, 存在失败: False
-	 */
-	public function isAllSigned()
-	{
-		return $this->allSigned;
-	}
+    /**
+     * 输出日志到文件
+     */
+    private function log()
+    {
+        file_put_contents(DIR_ROOT.'/sign.log', $this->logText, FILE_APPEND);
+    }
 
-	/**
-	 * 载入指定路径的文件
-	 * @param  string $filePath
-	 */
-	private function fileLoader($filePath)
-	{
-		if(!file_exists($filePath))
-			throw new Exception("文件丢失 ".$filePath, 1);
-		require_once($filePath);
-	}
+    /**
+     * 返回本次签到状态
+     * @return boolean 全部成功: True, 存在失败: False
+     */
+    public function isAllSigned()
+    {
+        return $this->allSigned;
+    }
+
+    /**
+     * 载入指定路径的文件
+     * @param  string $filePath
+     */
+    private function fileLoader($filePath)
+    {
+        if(!file_exists($filePath))
+            throw new Exception("文件丢失 ".$filePath, 1);
+
+        require_once($filePath);
+    }
 }
 
 ?>
--- sign.php
+++ sign.php
@@ -5,187 +5,187 @@
  */
 class Sign
 {
-	//日志状态
-	const SUCCESS     = '签到成功';
-	const SIGNED      = '今天已签到';
-	const FAILED      = '签到失败；';
-	const LOGINFAILED = '登录失败；';
+    //日志状态
+    const SUCCESS     = '签到成功';
+    const SIGNED      = '今天已签到';
+    const FAILED      = '签到失败；';
+    const LOGINFAILED = '登录失败；';
 
-	//用户名和密码
-	protected $username;
-	protected $password;
+    //用户名和密码
+    protected $username;
+    protected $password;
 
-	//cookie
-	protected $cookieDir =  DIR_PROT;
-	protected $cookieFile;
-	protected $cookieName;
+    //cookie
+    protected $cookieDir =  DIR_PROT;
+    protected $cookieFile;
+    protected $cookieName;
 
-	//输出日志内容
-	protected $logLine;
+    //输出日志内容
+    protected $logLine;
 
-	//CURL 选项
-	protected $curl_opts = array(
-			CURLOPT_RETURNTRANSFER => true,
-			CURLINFO_HEADER_OUT    => true,
-			CURLOPT_SSL_VERIFYPEER => false,
-			CURLOPT_SSL_VERIFYHOST => false,
-			CURLOPT_TIMEOUT        => 10,
-			CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.58 Safari/537.22'
-			);
+    //CURL 选项
+    protected $curl_opts = array(
+            CURLOPT_RETURNTRANSFER => true,
+            CURLINFO_HEADER_OUT    => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_TIMEOUT        => 10,
+            CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.58 Safari/537.22'
+            );
 
-	/**
-	 * 私有构造函数
-	 */
-	private function __construct()
-	{
-		
-	}
+    /**
+     * 私有构造函数
+     */
+    private function __construct()
+    {
+        
+    }
 
-	/**
-	 * 初始化配置信息
-	 * @param  string $username
-	 * @param  string $password
-	 * @return void          
-	 */
-	public function init($username, $password)
-	{
-		$this->username = $username;
-		$this->password = $password;
-		$this->cookieName = $this->preFix.$this->username;
-		$this->cookieFile = $this->cookieDir.$this->cookieName.'.cookie';
-		if(!file_exists($this->cookieFile))
-		{
-			fopen($this->cookieFile, 'w+');
-			$this->isCookieExist = false;
-		}
+    /**
+     * 初始化配置信息
+     * @param  string $username
+     * @param  string $password
+     * @return void          
+     */
+    public function init($username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+        $this->cookieName = $this->preFix.$this->username;
+        $this->cookieFile = $this->cookieDir.$this->cookieName.'.cookie';
+        if(!file_exists($this->cookieFile))
+        {
+            fopen($this->cookieFile, 'w+');
+            $this->isCookieExist = false;
+        }
 
-		$this->logLine = $this->cookieName.' ';
-	}
+        $this->logLine = $this->cookieName.' ';
+    }
 
-	/**
-	 * 发送 POST 请求
-	 * @param string  $url
-	 * @param array   $data POST 数据数组
-	 * @param array   $httpheader 构造 httpheader 内容的数组
-	 * @param boolean $header 是否输出 header 信息
-	 */
-	protected function post($url, $data = array(), $httpheader = array(), $header = false)
-	{
-		$options = $this->curl_opts;
-		$options[CURLOPT_URL] = $url;
-		$options[CURLOPT_POST] = true;
-		$options[CURLOPT_POSTFIELDS] = $data;
-		$options[CURLOPT_FOLLOWLOCATION] = true;
-		$options[CURLOPT_COOKIEJAR] = $this->cookieFile;
-		$options[CURLOPT_COOKIEFILE] = $this->cookieFile;
-		if(!empty($httpheader))
-			$options[CURLOPT_HTTPHEADER] = $httpheader;
-		if($header)
-			$options[CURLOPT_HEADER] = true;
-		return $this->curl($options);
-	}
+    /**
+     * 发送 POST 请求
+     * @param string  $url
+     * @param array   $data POST 数据数组
+     * @param array   $httpheader 构造 httpheader 内容的数组
+     * @param boolean $header 是否输出 header 信息
+     */
+    protected function post($url, $data = array(), $httpheader = array(), $header = false)
+    {
+        $options = $this->curl_opts;
+        $options[CURLOPT_URL] = $url;
+        $options[CURLOPT_POST] = true;
+        $options[CURLOPT_POSTFIELDS] = $data;
+        $options[CURLOPT_FOLLOWLOCATION] = true;
+        $options[CURLOPT_COOKIEJAR] = $this->cookieFile;
+        $options[CURLOPT_COOKIEFILE] = $this->cookieFile;
+        if(!empty($httpheader))
+            $options[CURLOPT_HTTPHEADER] = $httpheader;
+        if($header)
+            $options[CURLOPT_HEADER] = true;
+        return $this->curl($options);
+    }
 
-	/**
-	 * 发送 GET 请求
-	 * @param string  $url
-	 * @param array   httpheader 构造 httpheader 内容的数组
-	 * @param boolean $header 是否输出 header 信息
-	 */
-	protected function get($url, $httpheader = array(), $header = false)
-	{
-		$options = $this->curl_opts;
-		$options[CURLOPT_URL] = $url;
-		$options[CURLOPT_FOLLOWLOCATION] = true;
-		$options[CURLOPT_COOKIEJAR] = $this->cookieFile;
-		$options[CURLOPT_COOKIEFILE] = $this->cookieFile;
-		if(!empty($httpheader))
-			$options[CURLOPT_HTTPHEADER] = $httpheader;
-		if($header)
-			$options[CURLOPT_HEADER] = true;
-		return $this->curl($options);
-	}
+    /**
+     * 发送 GET 请求
+     * @param string  $url
+     * @param array   httpheader 构造 httpheader 内容的数组
+     * @param boolean $header 是否输出 header 信息
+     */
+    protected function get($url, $httpheader = array(), $header = false)
+    {
+        $options = $this->curl_opts;
+        $options[CURLOPT_URL] = $url;
+        $options[CURLOPT_FOLLOWLOCATION] = true;
+        $options[CURLOPT_COOKIEJAR] = $this->cookieFile;
+        $options[CURLOPT_COOKIEFILE] = $this->cookieFile;
+        if(!empty($httpheader))
+            $options[CURLOPT_HTTPHEADER] = $httpheader;
+        if($header)
+            $options[CURLOPT_HEADER] = true;
+        return $this->curl($options);
+    }
 
-	/**
-	 * curl 请求
-	 * @param  array $options curl 选项数组
-	 * @return string curl 响应结果
-	 */
-	protected function curl($options)
-	{
-		$ch = curl_init();
-		curl_setopt_array($ch, $options);
-		$result = curl_exec($ch);
-		// echo curl_getinfo($ch, CURLINFO_HEADER_OUT);
-		curl_close($ch);
-		return $result;
-	}
+    /**
+     * curl 请求
+     * @param  array $options curl 选项数组
+     * @return string curl 响应结果
+     */
+    protected function curl($options)
+    {
+        $ch = curl_init();
+        curl_setopt_array($ch, $options);
+        $result = curl_exec($ch);
+        // echo curl_getinfo($ch, CURLINFO_HEADER_OUT);
+        curl_close($ch);
+        return $result;
+    }
 
-	/**
-	 * 添加日志记录
-	 * @param string $log 日志内容
-	 */
-	public function appendLog($log)
-	{
-		$this->logLine .= $log;
-	}
+    /**
+     * 添加日志记录
+     * @param string $log 日志内容
+     */
+    public function appendLog($log)
+    {
+        $this->logLine .= $log;
+    }
 
-	/**
-	 * 输出日志
-	 */
-	public function getLog()
-	{
-		$this->logLine .= PHP_EOL;
-		echo $this->logLine,'<br>';
-		return $this->logLine;
-	}
+    /**
+     * 输出日志
+     */
+    public function getLog()
+    {
+        $this->logLine .= PHP_EOL;
+        echo $this->logLine,'<br>';
+        return $this->logLine;
+    }
 
-	/**
-	 * 删除 cookie，记录失败日志并抛出异常
-	 * @param int $errno 错误码 0:登录失败 1:签到失败 2:自定义日志
-	 * @param string $log 日志内容
-	 */
-	public function retry($errno = 1, $log = '')
-	{
-		switch ($errno)
-		{
-			case 0:
-				$this->logLine .= self::LOGINFAILED;
-				throw new Exception('Login failed', $errno);
-				break;
-			case 1:
-				@unlink($this->cookieFile);
-				$this->isCookieExist = false;
-				$this->logLine .= empty($log) ? self::FAILED : $log;
-				throw new Exception("Retry", $errno);
-				break;
-			case 2:
-				$this->logLine .= $log;
-				throw new Exception($log, $errno);
-				break;
-		}
-	}
+    /**
+     * 删除 cookie，记录失败日志并抛出异常
+     * @param int $errno 错误码 0:登录失败 1:签到失败 2:自定义日志
+     * @param string $log 日志内容
+     */
+    public function retry($errno = 1, $log = '')
+    {
+        switch ($errno)
+        {
+            case 0:
+                $this->logLine .= self::LOGINFAILED;
+                throw new Exception('Login failed', $errno);
+                break;
+            case 1:
+                @unlink($this->cookieFile);
+                $this->isCookieExist = false;
+                $this->logLine .= empty($log) ? self::FAILED : $log;
+                throw new Exception("Retry", $errno);
+                break;
+            case 2:
+                $this->logLine .= $log;
+                throw new Exception($log, $errno);
+                break;
+        }
+    }
 
-	/**
-	 * 获取子类的唯一实例
-	 * @return object
-	 */
-	public static function getInstance()
-	{
-		$childClass = get_called_class();
-		if(is_null($childClass::$_instance))
-			$childClass::$_instance = new $childClass;
+    /**
+     * 获取子类的唯一实例
+     * @return object
+     */
+    public static function getInstance()
+    {
+        $childClass = get_called_class();
+        if(is_null($childClass::$_instance))
+            $childClass::$_instance = new $childClass;
 
-		return $childClass::$_instance;
-	}
+        return $childClass::$_instance;
+    }
 
-	/**
-	 * 防止复制唯一实例
-	 * @return void
-	 */
-	public function __clone()
-	{
-		die('It\'s singleton pattern :)');
-	}
+    /**
+     * 防止复制唯一实例
+     * @return void
+     */
+    public function __clone()
+    {
+        die('It\'s singleton pattern :)');
+    }
 }
 
 ?>
